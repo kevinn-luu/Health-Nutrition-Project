1bdf0b9a0974abafa2d7049f925dd2b2de12d425
kevinn-luu <kevvluu1234@gmail.com>
2024-06-16 08:28:19 -0700

    create footer as test

--- health-nutrition-react-project/health-nutrition-react-app/src/App.jsx
+++ health-nutrition-react-project/health-nutrition-react-app/src/App.jsx
@@ -1,9 +1,13 @@
-import Header from "./Header.jsx"
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
 
 function App() {
   return(
     
-    <Header></Header>
+    <>
+      <Header></Header>
+      <Footer></Footer>
+    </>
     
   )
 }
