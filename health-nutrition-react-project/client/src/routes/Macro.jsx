0a0f30793453c169990bf2f8943e64ff56cda7d5
Brandon <jbcraft88@gmail.com>
2024-06-19 16:49:34 -0700

    added react router dom, added 3 routes, moved backend test to home page

---
+++ health-nutrition-react-project/client/src/routes/Macro.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Macro = () => {
+  return (
+    <div>DEEEEUIGH</div>
+  )
+}
+
+export default Macro
---
+++ health-nutrition-react-project/client/src/routes/Personal.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+
+const Personal = () => {
+  return (
+    <div>
+      <Link to="/personal/macro">CHECK UR MACROS BOZO</Link>
+    </div>
+  )
+}
+
+export default Personal
