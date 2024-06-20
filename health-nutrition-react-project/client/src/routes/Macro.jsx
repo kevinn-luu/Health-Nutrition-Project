0fe33dbe9d9c44e88bcc645c29a161753d5ec138
Brandon <jbcraft88@gmail.com>
2024-06-19 19:13:50 -0700

    added routes/pages with Home Macro and Personal

--- health-nutrition-react-project/client/src/routes/Macro.jsx
+++ health-nutrition-react-project/client/src/routes/Macro.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import axios from 'axios'
+import {useState, useEffect} from 'react'
 
 const Macro = () => {
+  const [loading, setLoading] = useState(false)
+  const [macros, setMacros] = useState([])
+
+  useEffect(() => {
+    setLoading(true)
+    axios
+      .get('http://localhost:5555/personal/macro')
+      .then((res) => {
+        setMacros(res.data.data)
+        setLoading(false)
+      })
+    .catch((err) => {
+      console.log(err); 
+    })
+  }, [])
+
   return (
-    <div>DEEEEUIGH</div>
+
+    <div>
+      {loading ? ( <p>DEEEEUIGH</p>) : (
+        // <div>
+        //   {
+        //     macros.map((macro) => {
+        //       <div key={macro._id} style={{border: "5px solid", margin: "10px"}}>
+        //         <p>
+        //           fats: {macro.fats}
+        //         </p>
+        //       </div>
+        //     })
+        //   }
+        // </div>
+        <p>{macros[0].fats}</p>
+      )}
+
+    </div>
   )
 }
 
