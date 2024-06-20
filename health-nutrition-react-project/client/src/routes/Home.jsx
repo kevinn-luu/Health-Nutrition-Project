a6cd656a1c71847681fe089a787436505616590e
kevinn-luu <kevvluu1234@gmail.com>
2024-06-19 20:10:41 -0700

    WE WON

--- health-nutrition-react-project/client/src/routes/Home.jsx
+++ health-nutrition-react-project/client/src/routes/Home.jsx
@@ -4,28 +4,11 @@ import api from "../services/api";
 import { useEffect, useState } from "react";
 
 const Home = () => {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await api.get('/data');
-        setData(response.data);
-      } catch (error) {
-        console.log(error.message);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
-    <div>
-      <h1>Connect frontend backend test</h1>
-      {data ? <h2>{data.message}</h2>: <h2>No message from backend</h2>}
+    <div> 
       <Link to="/personal">TO PERSONAL</Link>
     </div>
-  )
-}
+  );
+};
 
 export default Home
