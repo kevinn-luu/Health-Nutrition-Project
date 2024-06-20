fa47e97c31ce1e3b7b349e395e791b26232c8f3a
Brandon <jbcraft88@gmail.com>
2024-06-20 11:58:17 -0700

    added component and refacotred style sheet

--- health-nutrition-react-project/client/src/routes/Macro.jsx
+++ health-nutrition-react-project/client/src/routes/Macro.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import axios from 'axios'
 import {useState, useEffect} from 'react'
-import '../styles.css'
+import '../styles/macroPage.css'
+import Stats from "../components/macros/Stats.jsx"
 
 const Macro = () => {
   const [loading, setLoading] = React.useState(false)
@@ -26,24 +27,7 @@ const Macro = () => {
       {loading ? (
         <p>DEEEUIGH</p>
       ) : (
-        <div>
-          <h1>YOUR MACROS!</h1>
-          {macros.length > 0 ? (
-            macros.map((macro) => (
-              <div key={macro._id} className='macro--container'>
-                <p>fats: {macro.fats}g</p>
-                <p>cholesterol: {macro.cholesterol}g</p>
-                <p>sodium: {macro.sodium}g</p>
-                <p>fiber: {macro.fiber}g</p>
-                <p>sugar: {macro.sugar}g</p>
-                <p>protein: {macro.protein}g</p>
-                <p>time: {macro.updatedAt}</p>
-              </div>
-            ))
-          ) : (
-            <p>No macros available</p>
-          )}
-        </div>
+        <Stats macros={macros} />
       )}
 
     </div>
