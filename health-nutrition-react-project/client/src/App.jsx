2ec4ea5f971f1195462feceb92e62f626b3fb68c
kevinn-luu <kevvluu1234@gmail.com>
2024-06-24 17:53:03 -0700

    added layout component so nav button shows on every page

--- health-nutrition-react-project/client/src/App.jsx
+++ health-nutrition-react-project/client/src/App.jsx
@@ -6,20 +6,22 @@ import Personal from "./routes/Personal.jsx";
 import Macro from "./routes/Macro.jsx"
 import CalorieTrackerPage from "./routes/CalorieTrackerPage.jsx";
 import MacrosByDate from "./components/macros/MacrosByDate.jsx";
+import Layout from "./components/Layout.jsx";
 
 function app () {
 
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/personal" element={<Personal />} />
-        <Route path="/personal/macro" element={<Macro />} />
-        <Route path="/personal/calorie" element={<CalorieTrackerPage />} />
-        <Route path="/personal/macro/dates" element={<MacrosByDate />} />
-      </Routes>
-
-    </div>
+    <Layout>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/personal" element={<Personal />} />
+          <Route path="/personal/macro" element={<Macro />} />
+          <Route path="/personal/calorie" element={<CalorieTrackerPage />} />
+          <Route path="/personal/macro/dates" element={<MacrosByDate />} />
+        </Routes>
+      </div>
+    </Layout>
   )
 };
 
