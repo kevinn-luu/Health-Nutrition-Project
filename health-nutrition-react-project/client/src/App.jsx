6460d5ee4ac5e8cb7acab2e8f0020d84a7519fe3
Brandon <jbcraft88@gmail.com>
2024-06-24 18:37:45 -0700

    adding community shi

--- health-nutrition-react-project/client/src/App.jsx
+++ health-nutrition-react-project/client/src/App.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
-import api from "./services/api";
 import {Routes, Route} from 'react-router-dom'
 import Home from "./routes/Home.jsx"
 import Personal from "./routes/Personal.jsx";
 import Macro from "./routes/Macro.jsx"
 import CalorieTrackerPage from "./routes/CalorieTrackerPage.jsx";
 import MacrosByDate from "./components/macros/MacrosByDate.jsx";
+import Community from "./routes/Community.jsx";
 
 function app () {
 
@@ -17,6 +17,7 @@ function app () {
         <Route path="/personal/macro" element={<Macro />} />
         <Route path="/personal/calorie" element={<CalorieTrackerPage />} />
         <Route path="/personal/macro/dates" element={<MacrosByDate />} />
+        <Route path="/community" element={<Community />} />
       </Routes>
 
     </div>
